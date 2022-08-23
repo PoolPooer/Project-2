23ad07b7b619aeec504f57abb9ea566097c43906
Paul Power <power_604@msn.com>
2022-08-23 05:16:06 +0100

    Constructor class for bees
    Defining starting points for bees, sprite sheet source and initial code for hit detection

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
+//initalize canvas
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+//load background image
 const background = new Image();
-background.src = 'forest.png';
+background.src = 'assets/forest.png';
 
+//draw background on canvas
 function setBackground(){
   ctx.drawImage(background, 0,0);
   requestAnimationFrame(setBackground);
