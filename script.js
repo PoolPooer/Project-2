46a64ad60910588c40436b345ef58d88b7fbbb9c
Paul Power <power_604@msn.com>
2022-08-23 06:06:44 +0100

    Constructor class for bees
    assign random spawns, sizes, colours for bees

--- script.js
+++ script.js
@@ -16,3 +16,43 @@ background.src = 'assets/forest.png';
 let score = 0;
 let gameOver = false;
 ctx.font = '50px Impact';
+
+//time between bees
+let timeToNextBee = 0;
+let beeInterval = 500;
+let lastTime = 0;
+
+//constructor for creating the bees
+let bees = [];
+class Bee {
+  constructor(){
+    //sprite dimensions
+    this.spriteWidth = 247.5;
+    this.spriteHeight = 269;
+    //random size modifier for varied bees
+    this.sizeModifer = Math.random() * 0.6 + 0.4;
+    this.width = this.spriteWidth * this.sizeModifer;
+    this.height = this.spriteHeight * this.sizeModifer;
+    //bees start at right and will fly to the left side
+    this.x = canvas.width;
+    //bees start in full view on canvas and not partially obscured
+    this.y = Math.random() * (canvas.height - this.height);
+    //random generate spawn locations
+    this.directionX = Math.random() * 5 + 3;
+    this.directionY = Math.random() * 5 - 2.5;
+    // default state upon spawning in
+    this.markedForDeletion = false;
+    //load new bee image
+    this.image = new Image();
+    this.image.src = 'assets/bee.png';
+    //frame selection on sprite sheet
+    this.frame = 0;
+    this.maxFrame = 2;
+    //offset the animations between different bees
+    this.timeSinceFlap = 0;
+    this.flapInterval = Math.random() * 20 + 50;
+    //assign different colors on second canvas for hit dection upon mouse click
+    this.randomColors = [Math.floor(Math.random() * 255),Math.floor(Math.random() * 255),Math.floor(Math.random() * 255)];
+    this.color = 'rgb(' + this.randomColors[0] +  ',' + this.randomColors[1] +  ',' + this.randomColors[2] + ')';
+
+  }
