3c2f1904324ce1bc5c0747adc7b435b0be46b4cf
Paul Power <power_604@msn.com>
2022-08-23 05:52:22 +0100

    Addition of 2nd canvas
    2nd canvas to be used for hit detection. Score and Gameover state

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-//initalize canvas
+//initalize primary canvas
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+//second canvas using colour, will be hidden and used for hit decection
+const collisionCanvas = document.getElementById('collisionCanvas');
+const collisionCtx = collisionCanvas.getContext('2d');
+collisionCanvas.width = window.innerWidth;
+collisionCanvas.height = window.innerHeight;
 
 //load background image
 const background = new Image();
 background.src = 'assets/forest.png';
 
-//draw background on canvas
-function setBackground(){
-  ctx.drawImage(background, 0,0);
-  requestAnimationFrame(setBackground);
-};
-setBackground();
+let score = 0;
+let gameOver = false;
+ctx.font = '50px Impact';
